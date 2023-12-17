36f4d8a7e6e7c7cbc227f72e911d39df8993b712
peter86cu <39494828+peter86cu@users.noreply.github.com>
2023-12-17 18:03:19 -0300

    fg

--- src/main/java/com/online/multishop/ClientSslConfiguration.java
+++ src/main/java/com/online/multishop/ClientSslConfiguration.java
@@ -4,12 +4,16 @@ package com.online.multishop;
 
 import javax.net.ssl.SSLContext;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
-
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -19,7 +23,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ClientSslConfiguration {
 	
-	    @Value("${client.ssl.key-store}")
+	   @Value("${client.ssl.key-store}")
 	    private Resource trustStore;
 	    @Value("${client.ssl.key-store-password}")
 	    private String trustStorePassword;
@@ -47,4 +51,30 @@ public class ClientSslConfiguration {
 	        return new RestTemplate(factory);
 	    }
 
+	
+	/* @Bean
+	    public TomcatServletWebServerFactory servletContainer() {
+	        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+	            @Override
+	            protected void postProcessContext(Context context) {
+	                SecurityConstraint securityConstraint = new SecurityConstraint();
+	                securityConstraint.setUserConstraint("CONFIDENTIAL");
+	                SecurityCollection collection = new SecurityCollection();
+	                collection.addPattern("/*");
+	                securityConstraint.addCollection(collection);
+	                context.addConstraint(securityConstraint);
+	            }
+	        };
+	        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+	        return tomcat;
+	    }
+
+	    private Connector getHttpConnector() {
+	        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	        connector.setScheme("http");
+	        connector.setPort(8080);
+	        connector.setSecure(false);
+	        connector.setRedirectPort(8443);
+	        return connector;
+	    }*/
 }
